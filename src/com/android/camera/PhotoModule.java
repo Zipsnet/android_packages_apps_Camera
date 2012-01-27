bbd3e3e27aae74647ad0a0f047097873e6bbedea
Manogna Reddy Alle <cmalle@codeaurora.org>
2012-01-27 12:07:58 +0530

    Camera: Do not start autofocus when snapshot is in progress.
    (cherry picked from commit 9fbccbe5bad2249e13a86581ecbd1e68810a0e90)
    
    Change-Id: Id28c90b43ce33702d9c421e9a3b19d4502b77609

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -1927,9 +1927,11 @@ public class PhotoModule
 
     @Override
     public void autoFocus() {
-        mFocusStartTime = System.currentTimeMillis();
-        mCameraDevice.autoFocus(mAutoFocusCallback);
-        setCameraState(FOCUSING);
+        if(mCameraState != SNAPSHOT_IN_PROGRESS) {
+            mFocusStartTime = System.currentTimeMillis();
+            mCameraDevice.autoFocus(mAutoFocusCallback);
+            setCameraState(FOCUSING);
+        }
     }
 
     @Override
