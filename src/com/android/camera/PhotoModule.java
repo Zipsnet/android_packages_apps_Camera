6a39f043651888ba5d5c84e1a453d3766b072ee4
Bernd Bindreiter <bernd.bindreiter@gmail.com>
2013-01-17 22:50:16 +0100

    fixed HDR onscreen indicator

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -865,7 +865,7 @@ public class PhotoModule
             return;
         }
         if ((value == null) || Parameters.SCENE_MODE_AUTO.equals(value)
-                || Parameters.SCENE_MODE_HDR.equals(value)) {
+                || Util.SCENE_MODE_HDR.equals(value)) {
             mSceneIndicator.setImageResource(R.drawable.ic_indicator_sce_off);
         } else {
             mSceneIndicator.setImageResource(R.drawable.ic_indicator_sce_on);
@@ -876,7 +876,7 @@ public class PhotoModule
         if (mHdrIndicator == null) {
             return;
         }
-        if ((value != null) && Parameters.SCENE_MODE_HDR.equals(value)) {
+        if ((value != null) && Util.SCENE_MODE_HDR.equals(value)) {
             mHdrIndicator.setImageResource(R.drawable.ic_indicator_hdr_on);
         } else {
             mHdrIndicator.setImageResource(R.drawable.ic_indicator_hdr_off);
@@ -884,6 +884,7 @@ public class PhotoModule
     }
 
     private void updateOnScreenIndicators() {
+
         updateSceneOnScreenIndicator(mParameters.getSceneMode());
         updateExposureOnScreenIndicator(CameraSettings.readExposure(mPreferences));
         updateFlashOnScreenIndicator(mParameters.getFlashMode());
